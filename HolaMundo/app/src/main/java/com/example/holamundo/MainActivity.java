263d907247b75de1ca39f962dd4979f799a4100f
wacruz <62728969+wacruz@users.noreply.github.com>
2020-04-26 20:14:42 -0500

    update

--- HolaMundo/app/src/main/java/com/example/holamundo/MainActivity.java
+++ HolaMundo/app/src/main/java/com/example/holamundo/MainActivity.java
@@ -8,47 +8,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Toast;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
         //La actividad está creada.
     }
     public void siguiente(View view){
         Intent siguiente = new Intent(this, SegundaPantalla.class);
         startActivity(siguiente);
+        Toast.makeText(this,"Bienvenido",Toast.LENGTH_LONG).show();
     }
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
         // La actividad está a punto de hacerse visible.
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
         // La actividad se ha vuelto visible (ahora se "reanuda").
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
         // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
         // La actividad ya no es visible (ahora está "detenida")
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
         // La actividad está a punto de ser destruida.
     }
 
--- HolaMundo/app/src/main/java/com/example/holamundo/SegundaPantalla.java
+++ HolaMundo/app/src/main/java/com/example/holamundo/SegundaPantalla.java
@@ -2,13 +2,18 @@ package com.example.holamundo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class SegundaPantalla extends AppCompatActivity {
-
+private Button suma = new Button();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_pantalla);
     }
+
+
+
 }
